c83daac4d64c71bf7dc1f6a6258e82bec52da517
Jefferson Daniel <jefferson@dbr.ag>
2015-03-27 12:24:33 -0300

    initial commit

---
+++ scripts/mySlide.js
@@ -0,0 +1,154 @@
+(function($){
+
+  "use strict";
+
+  $.fn.fastSlide = function(defaults) {
+
+    // CONSIDERAR USAR PEGAR OPTIONS POR DATA ATRIBUTES, PARA O USUÁRIO NÃO PRECISAR TANTO EM JS.
+    var options = {
+      'dots':         true,
+      'time':         3000,
+      'autoPlay':     true,
+      'keyborad':     true,
+      'fullScreen':   false,
+      'currentClass': 'ative',
+    };
+
+    var settings = $.extend( {}, options, defaults );
+
+    var _this = this,
+        intervalSlide,
+        thisStopped = false,
+        current = 0,
+        $childBanner = $('.image', '.banner'),
+        $childDots = $('.dot', '.dots'),
+        $buttonNext = $('.next'),
+        $buttonPrevious = $('.previous'),
+        allChildren = $childBanner.length,
+        syncCurrentAndChild = allChildren - 1;
+
+    return this.each( function(){
+
+      function init() {
+        $childBanner.first().addClass(settings.currentClass);
+
+        if(settings.dots)
+          $childDots.first().addClass(settings.currentClass);
+
+        if(settings.fullScreen)
+          methods.fullScreen();
+
+        if(settings.autoPlay)
+          methods.play();
+
+        if(settings.keyborad)
+          methods.keyboard();
+
+        methods.mouseEvents();
+      }
+
+      var methods = {
+
+        fullScreen: function() {
+          if(settings.fullScreen) {
+            $('html').addClass('mySlide-fullscreen');
+          }
+        },
+
+        play : function() {
+          intervalSlide = setInterval( function(){
+            methods.nextSlide();
+          }, settings.time);
+        },
+
+        nextSlide: function() {
+          $childBanner.removeClass(settings.currentClass);
+          $childDots.removeClass(settings.currentClass);
+          current++;
+
+          if(allChildren === current) {
+            current = 0;
+          }
+
+          $childBanner.eq(current).addClass(settings.currentClass);
+          if(settings.dots)
+            $childDots.eq(current).addClass(settings.currentClass);
+        },
+
+        previousSlide: function() {
+          $childBanner.removeClass(settings.currentClass);
+          $childDots.removeClass(settings.currentClass);
+          current--;
+
+          if(current < 0) {
+            current = syncCurrentAndChild;
+          }
+
+          $childBanner.eq(current).addClass(settings.currentClass);
+          if(settings.dots)
+            $childDots.eq(current).addClass(settings.currentClass);
+        },
+
+        keyboard: function() {
+          $(document).bind('keyup', function(e){
+            if(e.which === 39) {
+              clearInterval(intervalSlide);
+              methods.nextSlide();
+
+              if(settings.autoPlay)
+                methods.play();
+
+            } else if(e.which === 37) {
+              clearInterval(intervalSlide);
+              methods.previousSlide();
+
+              if(settings.autoPlay)
+                methods.play();
+
+            } else if(e.which === 32) {
+              // fix - Arrunar para ele não reiniciar e sim voltar de onde parou
+              if(settings.autoPlay)
+
+                if(thisStopped){
+                  methods.play();
+                  console.log('play');
+                  thisStopped = false;
+
+                } else {
+                  clearInterval(intervalSlide);
+                  thisStopped = true;
+                  console.log('pause');
+
+                }
+
+            }
+          });
+        },
+
+        mouseEvents: function() {
+          $buttonNext.on('click', function(){
+            clearInterval(intervalSlide);
+            methods.nextSlide();
+
+            if(settings.autoPlay)
+              methods.play();
+          });
+
+          $buttonPrevious.on('click', function(){
+            clearInterval(intervalSlide);
+            methods.previousSlide();
+
+            if(settings.autoPlay)
+              methods.play();
+          });
+        }
+      }
+
+      // init all functions
+      init();
+
+    });
+
+  }
+
+})(jQuery);
